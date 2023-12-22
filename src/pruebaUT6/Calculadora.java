eed9ef5799757aece6b966fada7bc6020aa9ecfe
Juana Moya <j.moya@edu.gva.es>
2023-12-22 18:37:56 +0100

    Proyecto Calculadora para pruebas

---
+++ src/pruebaUT6/Calculadora.java
@@ -0,0 +1,56 @@
+package pruebaUT6;
+
+public class Calculadora {
+
+    private double memoria;
+
+    public double suma(double a, double b) {
+        this.memoria = a + b;
+        return this.memoria;
+    }
+
+    public double resta(double a, double b) {
+        this.memoria = a - b;
+        return this.memoria;
+    }
+
+    public double multiplicacion(double a, double b) {
+        this.memoria = a * b;
+        return this.memoria;
+    }
+
+    public double division(double a, double b) {
+        this.memoria = a / b;
+        return this.memoria;
+    }
+
+    public double absoluto(double a) {
+        if (a < 0) {
+            return -a;
+        } else {
+            return a;
+        }
+    }
+
+    public boolean esPar(int a) {
+        return a % 2 == 0;
+    }
+
+    public void inicializaMemoria() {
+        this.memoria = 0;
+    }
+
+    public double getMemoria() {
+        return this.memoria;
+    }
+
+    public int redondearDefecto(double d) {
+        int result = (int) d;
+        if (d < 0) {
+            result--;
+        }
+
+        return result;
+
+    }
+}
